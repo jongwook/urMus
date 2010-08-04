571e3ab75faadbd4d4b03afeb96cc5806d52e116
Jong Wook Kim <jongwook@umich.edu>
2010-08-04 16:43:18 -0400

    reads sizes of files in assets dir

--- SystemDependent/Android/src/edu/umich/urMus/urMus.java
+++ SystemDependent/Android/src/edu/umich/urMus/urMus.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.widget.TextView;
 import android.util.Log;
 import android.content.res.AssetManager;
+import android.content.res.AssetFileDescriptor;
 
+import java.io.InputStream;
 import java.io.IOException;
 
 public class urMus extends Activity
@@ -39,17 +41,32 @@ public class urMus extends Activity
 	public static native void changeBackground();
 
 	public void install() {
-		AssetManager assetManager = getAssets();
-        String[] files = null;
-        try {
-            files = assetManager.list("html");
-        } catch (IOException e) {
-            Log.e(TAG, e.getMessage());
-        }
-		if(files!=null) {
-			for(String file : files) {
-				Log.i("opening : %s",file);
-				
+		AssetManager assets = getAssets();
+		String storagePath=getFilesDir().getAbsolutePath();
+		Log.i(TAG,"Storage Path : " + storagePath);
+		// source directories in assets directory
+		String sources[]={"html","html/js","html/css","Sounds","Textures","urMus-Lua"};
+		for(String source : sources) {
+			Log.i(TAG,"traversing "+source);
+			String[] files = null;
+			try {
+				files = assets.list(source);
+			} catch (IOException e) {
+				Log.e(TAG, e.getMessage());
+			}
+			if(files!=null) {
+				for(String file : files) {
+					String filepath=source+"/"+file;
+					Log.i(TAG,"opening : "+filepath);
+					try {
+						InputStream input=assets.open(filepath,AssetManager.ACCESS_STREAMING);
+						int cnt=0, data;
+						while((data=input.read())!=-1) cnt++;
+						Log.i(TAG, "File size : "+cnt);
+					} catch (IOException e) {
+						Log.e(TAG, e.getMessage());
+					}
+				}
 			}
 		}
 	}
